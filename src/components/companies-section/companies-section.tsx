5c71c031ce48bcc494c64603c84a8bf9670c1c4b
Albert Barsegyan <albert.barseghyan@banzai.io>
2024-03-07 08:41:54 +0400

    feat: add CompaniesSection

--- src/components/companies-section/companies-section.tsx
+++ src/components/companies-section/companies-section.tsx
@@ -1,7 +1,29 @@
+import { Fragment } from 'react';
+
+const Companies = [
+  { id:0,
+    content:'First company'
+  },
+  { id:1,
+    content:'Second Company'
+  },
+  { id:2,
+    content:'Third company'
+  }
+];
+
 export function CompaniesSection() {
   return (
-    <>
+    <div>
+      <div className='rounded-full w-full bg-blue-gray-900 py-20 flex flex-col justify-center items-center'>
+        <span>TECHNOLOGIES & HARDWARE</span>
+        <p>USED BY HYDRA VR.</p>
+        <div></div>
+      </div>
 
-    </>
+      <div>
+        {Companies.map(company => <Fragment key={company.id}>{company.content}</Fragment>)}
+      </div>
+    </div>
   );
 };
